0b203d6ab88dcda192c74a1f8657b1a7f2ea2aad
DimaD1989 <d_d1989sv@mail.ru>
2022-09-04 17:03:28 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,53 @@
-﻿Console.Write("Введи N: ");
-int N = Convert.ToInt16(Console.ReadLine());
-Console.Write("Введи M: ");
-int M = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine($"N={N}, M={M}");
-if (M>N)
-for (int i = N; i <= M; i++)
-    Console.Write($" {i}");
-else
-    for (int i = M; i <= N; i++)
-        Console.Write($" {i}");
+﻿// Задайте значения M и N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""1
+// Console.Write("Введи N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"N={N}, M={M}");
+// if (M>N)
+// for (int i = N; i <= M; i++)
+//     Console.Write($" {i}");
+// else
+//     for (int i = M; i <= N; i++)
+//         Console.Write($" {i}");
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// int m = InputNumbers("Введите m: ");
+// int n = InputNumbers("Введите n: ");
+// int temp = m;
+
+// if (m > n) 
+// {
+//   m = n; 
+//   n = temp;
+// }
+
+// PrintSumm(m, n, temp=0);
+
+// void PrintSumm(int m, int n, int summ)
+// {
+//   summ = summ + n;
+//   if (n <= m)
+//   {
+//     Console.Write($"Сумма элементов= {summ} ");
+//     return;
+//   }
+//   PrintSumm(m, n - 1, summ);
+// }
+
+// int InputNumbers(string input) 
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
